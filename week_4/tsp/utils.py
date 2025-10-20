aba196d17cbfa65a3e2613f49278df06a72ae5e4
lqrz <lautaro.quiroz@gmail.com>
2025-10-20 18:48:16 -0300

    restructure

--- week_4/tsp/utils.py
+++
@@ -1,9 +0,0 @@
-import math
-from collections import namedtuple
-
-
-Point = namedtuple("Point", ['x', 'y'])
-
-
-def length(point1: Point, point2: Point) -> float:
-    return math.sqrt((point1.x - point2.x) ** 2 + (point1.y - point2.y) ** 2)
